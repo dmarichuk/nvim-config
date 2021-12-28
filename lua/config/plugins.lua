4aba0ec5e340c3f25a8cd614a73848e84e3efa06
Daniil Marichuk <d.marichuk@olamnet.com>
2021-12-28 15:26:19 +0300

    lsp & telescope

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -52,13 +52,24 @@ return packer.startup(function(use)
     use "hrsh7th/cmp-path" -- path completions
     use "hrsh7th/cmp-cmdline" -- cmdline completions
     use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    use "hrsh7th/cmp-nvim-lsp" -- lsp completions
+    use "hrsh7th/cmp-nvim-lua" -- lua completion
 
     -- snippets
     use "L3MON4D3/LuaSnip" --snippet engine
     use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+    -- LSP
+    use "neovim/nvim-lspconfig" -- enable LSP
+    use "williamboman/nvim-lsp-installer" -- language server installer
+
+    --Telescope
+    use "nvim-telescope/telescope.nvim"
+    use 'nvim-telescope/telescope-media-files.nvim'
+
     -- Colorscheme
     use 'shaunsingh/nord.nvim'
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
